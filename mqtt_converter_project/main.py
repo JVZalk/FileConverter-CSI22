114b5d6dee2bee2db211392a65038226ac723a81
João Victor Zalkauskas <Joaovictorzmo@gmail.com>
2025-06-17 10:42:27 -0300

    Refatoracao da main e MQTT para encapsular leitura

--- mqtt_converter_project/main.py
+++ mqtt_converter_project/main.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 from src.core.mqtt_packet import MqttPacket
 
+#Leitura dos arquivos
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 config_path = os.path.join(base_dir, 'config', 'settings.json')
@@ -16,18 +17,6 @@ input_path = os.path.join(base_dir, 'input', 'example_packet.json')
 with open(input_path, 'r') as f:
     dados_do_pacote = json.load(f)
 
-pacote = MqttPacket(
-    payload=dados_do_pacote['payload'],
-    payload_format=dados_do_pacote.get('contentType', 'unknown'),
-    packet_type=dados_do_pacote.get('packetType'),
-    packet_id=dados_do_pacote.get('packetId'),
-    topic_name=dados_do_pacote.get('topicName'),
-    payload_format_indicator=dados_do_pacote.get('payloadFormatIndicator'),
-    content_type=dados_do_pacote.get('contentType'),
-    qos=dados_do_pacote.get('qos'),
-    retain_flag=dados_do_pacote.get('retainFlag')
-)
-
+# Criação do pacote
+pacote = MqttPacket.from_dict(dados_do_pacote)
 print(pacote)
-
-
