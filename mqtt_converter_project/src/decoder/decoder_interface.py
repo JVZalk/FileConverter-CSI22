1348987959651d56f696df3ecd1dccb86bcd1b0f
João Victor Zalkauskas <Joaovictorzmo@gmail.com>
2025-06-17 10:13:02 -0300

    decoder_interface para definir regra de decode

--- mqtt_converter_project/src/decoder/decoder_interface.py
+++ mqtt_converter_project/src/decoder/decoder_interface.py
@@ -1,9 +1,16 @@
 from abc import ABC, abstractmethod
 
+from src.core.standard_payload import StandardPayload 
 class PayloadDecoder(ABC):
-    """Define o contrato para todos os decodificadores. Padrão: Strategy (Interface)."""
+    
+    """   
+    Define o contrato para todos os decodificadores. 
+    Padrão: Strategy.
+    """
 
     @abstractmethod
-    def decode(self, payload: str):
-        """Método abstrato para decodificar um payload de string para um formato padrão."""
+    def decode(self, payload: str) -> StandardPayload:
+        """
+        Método abstrato obrigatório para decodificar um payload (string) e transformá-lo em um StandardPayload (dicionário).
+        """
         pass
