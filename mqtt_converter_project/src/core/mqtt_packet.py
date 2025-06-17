bde38eac44464420deae04d3b06b1e7fc9668afa
thiagoufrota <thiagoufrota@gmail.com>
2025-06-17 11:58:50 -0300

    __init__ mqtt_packet

--- mqtt_converter_project/src/core/mqtt_packet.py
+++ mqtt_converter_project/src/core/mqtt_packet.py
@@ -1,38 +1,16 @@
 # src/core/mqtt_packet.py
 
 class MqttPacket:
-    def __init__(self, payload: any, packet_type: str = None, 
-                 packet_id: str = None, topic_name: str = None, 
-                 payload_format_indicator: int = None, content_type: str = None, 
-                 qos: int = None, retain_flag: bool = None):
-        self.payload = payload
-        self.packet_type = packet_type
-        self.packet_id = packet_id
-        self.topic_name = topic_name
-        self.payload_format_indicator = payload_format_indicator
-        self.content_type = content_type
-        self.qos = qos
-        self.retain_flag = retain_flag
-
-    @classmethod
-    def from_dict(cls, data: dict):
-        """
-        [FACTORY METHOD]
-        Este método de fábrica é responsável por criar uma instância de MqttPacket
-        a partir de um dicionário (vindo de um JSON).
-        Toda a lógica de extrair os campos fica encapsulada aqui.
-        """
-        print("⚙️  Usando o Factory Method 'from_dict' para criar o pacote...")
-        return cls(
-            payload=data.get('payload'),
-            packet_type=data.get('packetType'),
-            packet_id=data.get('packetId'),
-            topic_name=data.get('topicName'),
-            payload_format_indicator=data.get('payloadFormatIndicator'),
-            content_type=data.get('contentType'),
-            qos=data.get('qos'),
-            retain_flag=data.get('retainFlag')
-        )
+    def __init__(self, data: dict):
+        self.packet_type = data.get('packetType', None)
+        self.dup = data.get('dup', None)
+        self.qos = data.get('qos', None)
+        self.retain_flag = data.get('retainFlag', None)
+        self.topic_name = data.get('topicName', None)
+        self.packet_id = data.get('packetId', None)
+        self.payload_format_indicator = data.get('payloadFormatIndicator', None)
+        self.content_type = data.get('contentType', None)
+        self.payload = data.get('payload', None)
 
     def __repr__(self):
         return (f"MqttPacket(payload={self.payload}, formato='{self.content_type}', "
