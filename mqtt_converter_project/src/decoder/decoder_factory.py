3e424c381fed78a1f4c3cbfcc767848c853933b1
João Victor Zalkauskas <Joaovictorzmo@gmail.com>
2025-06-17 10:19:29 -0300

    decoder factorypara chamar os decoders especificos

--- mqtt_converter_project/src/decoder/decoder_factory.py
+++ mqtt_converter_project/src/decoder/decoder_factory.py
@@ -1,11 +1,24 @@
-# from .decoder_interface import PayloadDecoder
-# from .json_decoder import JsonDecoder
-# ... outros imports
+
+from .decoder_interface import PayloadDecoder
+from .json_decoder import JsonDecoder
+from .csv_decoder import CsvDecoder
 
 class DecoderFactory:
-    """Classe que cria a instância do decodificador correto."""
-
+    """
+    Cria e retorna a instância do decodificador correto.
+    Padrão: Factory Method.
+    """
+    
     @staticmethod
-    def get_decoder(format_name: str): # -> PayloadDecoder
-        """Método para retornar o decodificador apropriado. Padrão: Factory Method."""
-        pass
+    def get_decoder(format_name: str) -> PayloadDecoder:
+
+        """
+        Recebe o nome de um formato e retorna o objeto decodificador correspondente.
+        """
+        if format_name.lower() == 'json':
+            return JsonDecoder()
+        elif format_name.lower() == 'csv':
+            return CsvDecoder()
+
+        else:
+            raise ValueError(f"Formato de decodificador não suportado: '{format_name}'")
