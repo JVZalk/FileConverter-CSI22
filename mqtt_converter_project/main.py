d164306d5d8fb921ccf24be65889861910b3a5cd
João Victor Zalkauskas <Joaovictorzmo@gmail.com>
2025-06-17 11:01:38 -0300

    adicionando comandos da main para testar o decoder

--- mqtt_converter_project/main.py
+++ mqtt_converter_project/main.py
@@ -4,6 +4,7 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 from src.core.mqtt_packet import MqttPacket
+from src.decoder.decoder_factory import DecoderFactory
 
 #Leitura dos arquivos
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -20,3 +21,22 @@ with open(input_path, 'r') as f:
 # Criação do pacote
 pacote = MqttPacket.from_dict(dados_do_pacote)
 print(pacote)
+
+# DECODER
+
+formato_do_payload = pacote.content_type
+payload_bruto = pacote.payload
+
+try:
+    decoder = DecoderFactory.get_decoder(formato_do_payload)
+    print(f"Fábrica selecionou o decodificador: {type(decoder).__name__}")
+
+    dados_padronizados = decoder.decode(payload_bruto)
+    
+    print("\nPayload decodificado:")
+    print(dados_padronizados)
+
+except ValueError as e:
+    print(f"\nErro durante a decodificação: {e}")
+except Exception as e:
+    print(f"\nOcorreu um erro inesperado: {e}")
