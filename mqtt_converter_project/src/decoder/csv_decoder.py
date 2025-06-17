1589003011465c418871e938a7cb2b82433f923d
João Victor Zalkauskas <Joaovictorzmo@gmail.com>
2025-06-17 11:28:41 -0300

    novo example_packet

--- mqtt_converter_project/src/decoder/csv_decoder.py
+++ mqtt_converter_project/src/decoder/csv_decoder.py
@@ -1,9 +1,23 @@
-# from .decoder_interface import PayloadDecoder
-# from src.core.standard_payload import StandardPayload
+import csv
+import io
+from .decoder_interface import PayloadDecoder, StandardPayload
 
-class CsvDecoder: # (PayloadDecoder)
-    """Classe que implementa a decodificação de CSV. Padrão: Strategy (Implementação Concreta)."""
+class CsvDecoder(PayloadDecoder):
+    """
+    Implementa a decodificação para o formato CSV.
+    Padrão: Strategy Concreta).
+    """
 
-    def decode(self, payload: str): # -> StandardPayload
-        """Método para decodificar um payload no formato CSV."""
-        pass
+    def decode(self, payload: str) -> StandardPayload:
+
+        try:
+            # Usa io.StringIO para tratar a string do payload como se fosse um arquivo
+            csv_file = io.StringIO(payload)
+            # DictReader lê o CSV e já usa o cabeçalho para criar dicionários
+            reader = csv.DictReader(csv_file)
+            # Pega o primeiro dicionário da lista (a primeira linha de dados)
+            first_row = next(reader, None)
+            return first_row if first_row is not None else {}
+        except Exception as e:
+            print(f"Erro ao decodificar CSV: {e}")
+            return {}
