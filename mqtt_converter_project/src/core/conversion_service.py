b47cfd5dc2c37479f749bb0bcc6769fb148bff3e
João Victor Zalkauskas <Joaovictorzmo@gmail.com>
2025-06-17 16:48:20 -0300

    alterando conversion_service para arquivo ser gerado na pasta correta

--- mqtt_converter_project/src/core/conversion_service.py
+++ mqtt_converter_project/src/core/conversion_service.py
@@ -1,3 +1,4 @@
+import os
 from ..decoder.decoder_factory import DecoderFactory
 from ..encoder.encoder_factory import EncoderFactory
 from src.core.mqtt_packet import MqttPacket
@@ -12,9 +13,9 @@ class ConversionService:
         """
         Executa a lógica de decodificação e codificação que antes estava na main.py.
         """
-        target_format = config.get('target_format', 'txt') # Pega o formato alvo do config
+        target_format = config.get('target_format', 'txt')
+        output_path = config.get('output_path', '.')  # <-- pega o output_path do settings
 
-        # --- Bloco DECODER (movido da main.py) ---
         formato_do_payload = packet.content_type
         payload_bruto = packet.payload
 
@@ -29,27 +30,32 @@ class ConversionService:
 
         except ValueError as e:
             print(f"\nErro durante a decodificação: {e}")
-            return # Interrompe a execução em caso de erro
+            return
         except Exception as e:
             print(f"\nOcorreu um erro inesperado: {e}")
-            return # Interrompe a execução em caso de erro
+            return
 
-        # --- Bloco ENCODER (movido da main.py) ---
         try:
             encoder = EncoderFactory.get_encoder(target_format)
             print(f"\nFábrica selecionou o codificador: {type(encoder).__name__}")
 
-            # Codifica os dados padronizados em um arquivo
             conteudo = encoder.encode(dados_padronizados)
             print(conteudo)
 
             print(f"\nDados codificados com sucesso no formato '{target_format}'.")
+
+             # Torna o caminho absoluto em relação ao diretório do main.py
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            project_root = os.path.abspath(os.path.join(base_dir, '..', '..'))
+            output_dir = os.path.join(project_root, output_path)
+
             file_name = f"output_data.{target_format}"
-            FileExporter.export(conteudo, file_name)       # Salva o arquivo
+            full_output_path = os.path.join(output_dir, file_name)
+
+            os.makedirs(output_dir, exist_ok=True)
+            FileExporter.export(conteudo, full_output_path)
 
         except ValueError as e:
             print(f"\nErro durante a codificação: {e}")
         except Exception as e:
             print(f"\nOcorreu um erro inesperado: {e}")
-
-        
