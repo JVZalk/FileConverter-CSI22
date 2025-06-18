bebe32d2f26a642ec100de45c95e9c07682ca4aa
Brunnorezendes <brunnorezendes@gmail.com>
2025-06-17 21:45:37 -0300

    ultimo alinhamento no codigo auxiliar para apresentacao

--- test_converter.py
+++ test_converter.py
@@ -1,7 +1,5 @@
 import os
 import json
-
-# A importação crucial da sua função universal a partir do seu pacote de projeto
 from mqtt_converter_project.main import convert_to
 
 mqtt_message_str = """
@@ -11,14 +9,30 @@ mqtt_message_str = """
   "qos": 1,
   "retainFlag": false,
   "topicName": "dispositivos/sensores",
-  "packetId": "data-dat-001",
+  "packetId": "data-json-001",
   "payloadFormatIndicator": 1,
-  "contentType": "dat",
-  "payload": "sensor_id;timestamp;temperatura;umidade;pressao\\n123;2024-06-17T10:00:00Z;25.3;60.2;1013.1\\n124;2024-06-17T10:01:00Z;24.8;61.0;1012.8"
+  "contentType": "json",
+  "payload": {
+    "timestamp": "2024-06-17T10:00:00Z",
+    "leituras": [
+      {
+        "sensor_id": 123,
+        "temperatura": 25.3,
+        "umidade": 60.2,
+        "pressao": 1013.1
+      },
+      {
+        "sensor_id": 124,
+        "temperatura": 24.8,
+        "umidade": 61.0,
+        "pressao": 1012.8
+      }
+    ]
+  }
 }
 """
 
 # --- TESTE 1: Conversão simples para XML ---
-print("--- TESTE 1: CSV -> XML (sem flags) ---")
+print("--- TESTE: ---")
 xml_output = convert_to(mqtt_message_str, 'json')
 print("Resultado:\n", xml_output)
